3b958674ef71eb932b5a1f96b0b089556c1e4714
Nai <corsaiir@gmail.com>
2022-04-13 03:05:30 -0300

    Shared

--- Frontend/Angular/e-commerce/src/app/shared/shared.module.ts
+++ Frontend/Angular/e-commerce/src/app/shared/shared.module.ts
@@ -1,12 +1,33 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { WordWrapPipe } from './pipes/word-wrap.pipe';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WordWrapPipe
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatMenuModule,
+    RouterModule
+  ],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTooltipModule
   ]
 })
 export class SharedModule { }
